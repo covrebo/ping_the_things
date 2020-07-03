1df147d3c36357b9cfcd6b71ed9de107ebc7fe64
Chris Ovrebo <chris.ovrebo@gmail.com>
2020-07-02 21:18:17 -0500

    Initial commit.

---
+++ ping_the_things.py
@@ -0,0 +1,28 @@
+import os
+
+hosts = ['192.168.1.1', 'google.com']
+report = []
+
+for host in hosts:
+    response = os.system(f'ping -c 1 {host}')
+
+    if response == 0:
+        report.append([f'{host} is up!', 1])
+    else:
+        report.append([f'{host} is down!', 0])
+
+print('-' * 40)
+
+print('These hosts are down:')
+for result in report:
+    if result[1] == 0:
+        print(f'\t {result[0]}')
+
+print('-' * 40)
+
+print('These hosts are up:')
+for result in report:
+    if result[1] == 1:
+        print(f'\t {result[0]}')
+
+print('-' * 40)
